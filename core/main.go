0213ccfcb76a52bcc45528c4dbf5f49acaa391df
William Kurkian <wkurkian3@gmail.com>
2019-06-15 11:14:07 -0400

    Basic core streaming world

--- core/main.go
+++ core/main.go
@@ -1,42 +1,41 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
 	"github.com/willkurk/distgame/protos"
 	"google.golang.org/grpc"
 	"log"
 	"net"
-	"net/http"
-	"time"
 )
+
+var world []*protos.World
 
 type worldServer struct {
 	World *protos.World
 }
 
-func (s *worldServer) WorldDownload(req *protos.WorldRequest, stream protos.World_WorldDownloadServer) error {
-	log.Output(1, "World Start initiated")
-	return s.clientStream(stream)
+func (s *worldServer) WorldStartListen(req *protos.WorldRequest, stream protos.WorldListen_WorldStartListenServer) error {
+	log.Output(1, "World Listen initiated")
+	//return s.clientStream(stream)
+	return nil
 }
 
 // our main function
 func main() {
-	world1 := &protos.World{Id: 1, Entity: nil}
-	world1.Entity = make([]*protos.Entity, 0)
-	world1.Entity = append(world1.Entity, &protos.Entity{Type: "grass", Area: &protos.Rect2D{X: 0, Y: 0, Width: 1000, Height: 1000}})
-	world1.Entity = append(world1.Entity, &protos.Entity{Type: "villager", Area: &protos.Rect2D{X: 100, Y: 100, Width: 50, Height: 50}})
+	world1 := &protos.World{Id: 1, Terrain: nil, Actor: nil}
+	world1.Terrain = make([]*protos.Terrain, 0)
+	world1.Terrain = append(world1.Terrain, &protos.Terrain{Type: "grass", Area: &protos.Rect2D{X: 0, Y: 0, Width: 1000, Height: 1000}})
+	world1.Actor = make([]*protos.Actor, 0)
+	world1.Actor = append(world1.Actor, &protos.Actor{Type: "villager", Area: &protos.Rect2D{X: 100, Y: 100, Width: 50, Height: 50}})
 	world = append(world, world1)
 
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 55600))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 55500))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
-	protos.RegisterWorldSyncServer(grpcServer, &worldServer{})
+	protos.RegisterWorldListenServer(grpcServer, &worldServer{})
 	grpcServer.Serve(lis)
 }
